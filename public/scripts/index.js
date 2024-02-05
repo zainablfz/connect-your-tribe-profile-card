ca71e288026c90e7c631c9c747bcae49d1e96e01
zainablfz <144009548+zainablfz@users.noreply.github.com>
2024-02-05 20:51:32 +0100

    v1
    oude visitekaartje

---
+++ public/scripts/index.js
@@ -0,0 +1,12 @@
+const article = document.getElementById('article-front')
+const rotatebutton = document.querySelector('.button')
+const articleback = document.querySelector('.article-back')
+
+rotatebutton.addEventListener ('click', () => {
+    article.classList.toggle('rotated');
+
+    if (article.classList.contains('rotated')) {
+  
+      }
+});
+// })
